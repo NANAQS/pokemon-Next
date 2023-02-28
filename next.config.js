8825c2c359c02c69285910437d7e65420a476673
NANAQS <otavioanime1@gmail.com>
2023-02-28 18:41:49 -0300

    base projeto

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    largePageDataBytes: 128 * 100000,
+  },
+  images: {
+    domains: ["raw.githubusercontent.com"]
+  }
 }
 
 module.exports = nextConfig
